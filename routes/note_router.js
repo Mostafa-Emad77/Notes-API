e5479ec7bf373710c0a55048b3b7c40ba468958a
Ibrahim Al-Gazzar <94261285+IbrahimGazzar@users.noreply.github.com>
2023-04-13 23:54:42 +0200

    Add files via upload

--- routes/note_router.js
+++ routes/note_router.js
@@ -3,12 +3,12 @@ const noteRouter = Router()
 
 const notesController = require('../controllers/note.controller');
 
-noteRouter.get('/', notesController.getALLNotes);
+noteRouter.get('/', notesController.getALLNote);
 noteRouter.get('/:id', notesController.getNotebyId);
 noteRouter.post('/', notesController.addNote);
 noteRouter.put('/:id', notesController.editnotebyID);
 noteRouter.delete('/:id', notesController.deletenote);
-noteRouter.post('/search', notesController.searchNotes);
+noteRouter.post('/search', notesController.searchNote);
 
 
 module.exports = {
