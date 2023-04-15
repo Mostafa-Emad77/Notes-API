8a52847683b7cf68313d6bef9cbbbabd5b4fb512
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-04-16 00:40:33 +0200

    Added Search endpoint

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -81,7 +81,6 @@ const deletenote = async (req,res) => {
 module.exports={
     getALLNote,
     getNotebyId,
-    searchNote,
     editnotebyID,
     addNote,
     deletenote
--- routes/note_router.js
+++ routes/note_router.js
@@ -2,15 +2,14 @@ const { Router } = require('express');
 const noteRouter = Router()
 
 const notesController = require('../controllers/note.controller');
+const Search = require('../features/Search');
 
 noteRouter.get('/', notesController.getALLNote);
 noteRouter.get('/:id', notesController.getNotebyId);
 noteRouter.post('/', notesController.addNote);
 noteRouter.put('/:id', notesController.editnotebyID);
 noteRouter.delete('/:id', notesController.deletenote);
-noteRouter.post('/search', notesController.searchNote);
-
-
+noteRouter.post('/search', Search.searchNote);
 module.exports = {
     noteRouter
 };
