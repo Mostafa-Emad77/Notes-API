1e29c4ddba1ab57b6c50105bcd75dc2ac8fbf3b0
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-04-18 06:02:09 +0200

    Fixing errors

--- routes/note_router.js
+++ routes/note_router.js
@@ -1,22 +1,3 @@
-<<<<<<< Updated upstream
-const { Router } = require('express');
-const noteRouter = Router()
-
-const notesController = require('../controllers/note.controller');
-const Search = require('../features/Search');
-
-noteRouter.get('/', notesController.getALLNote);
-noteRouter.get('/:id', notesController.getNotebyId);
-noteRouter.post('/', notesController.addNote);
-noteRouter.put('/:id', notesController.editnotebyID);
-noteRouter.delete('/:id', notesController.deletenote);
-noteRouter.post('/search', notesController.searchNote);
-noteRouter.post('/template',notesController.addWithTemplate);
-
-module.exports = {
-    noteRouter
-};
-=======
 const { Router } = require('express');
 const noteRouter = Router()
 
@@ -39,4 +20,4 @@ noteRouter.put('/archive/:id', notesController.archiveNote);
 module.exports = {
     noteRouter
 };
->>>>>>> Stashed changes
+
