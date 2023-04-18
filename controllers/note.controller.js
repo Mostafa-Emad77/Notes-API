25b56426a154469f6d64d54dd8b1fd2d38c15bde
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-04-18 06:00:12 +0200

    fixing some errors

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -1,80 +1,3 @@
-<<<<<<< Updated upstream
-const { validateNote} = require('../helper/validation');
-const {addWithTemplate} = require('../features/template');
-const {searchNote} = require('../features/Search');
-const { Note } = require('../models/note');
-const getALLNote = async (req, res) => {
-    const notes = await Note.find().sort({ pinned: -1 });
-    res.send(notes);
-  };
-
-  const getNotebyId = async (req, res) => {
-    try {
-      const note = await Note.findById(req.params.id);
-      res.status(200).send(note);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
-  
-const addNote = async (req, res) => {
-    try {
-      const result = validateNote(req.body);
-      if (result) {
-        const note = await Note.create(req.body);
-        res.status(200).send(note);
-      }
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
-
-const editnotebyID = async (req,res) => {
-    try{
-        const note = await Note.findOne({_id: req.params.id});
-        if(!note){
-            return res.status(404).send('Note not found');
-        }
-        const result = validateNote(req.body);
-        if(result){
-            if(req.body.name){
-                    note.name = req.body.name;
-           }
-            if(req.body.description){
-                note.description = req.body.description;
-            }
-            if(req.body.dueDate){
-              note.dueDate = req.body.dueDate;
-            }
-            await note.save();
-            res.status(200).send(note);
-        }
-    }
-    catch(error){
-        res.status(400).send(error);
-    }
-};
-
-const deletenote = async (req,res) => {
-    try{
-        const note = await Note.deleteOne({_id: req.params.id});
-        res.status(200).send();
-    }
-    catch(error){
-        res.status(400).send(error);
-    }
-};  
-
-module.exports={
-    getALLNote,
-    getNotebyId,
-    editnotebyID,
-    searchNote,
-    addNote,
-    deletenote,
-    addWithTemplate
-}
-=======
 const { validateNote} = require('../helper/validation');
 const {addWithTemplate} = require('../features/template');
 const {searchNote} = require('../features/Search');
@@ -154,4 +77,4 @@ module.exports={
     archiveNote,
     getAllArchiveNotes
 }
->>>>>>> Stashed changes
+
