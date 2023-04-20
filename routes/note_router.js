7a4829cb6e99bbb39de25ff2c2c50eb1d184f9b9
Abdullah <abdullah180463@feng.bu.edu.eg>
2023-04-20 02:02:42 +0200

    adding export feature

--- routes/note_router.js
+++ routes/note_router.js
@@ -2,16 +2,20 @@ const { Router } = require('express');
 const noteRouter = Router()
 
 const notesController = require('../controllers/note.controller');
-const Search = require('../features/Search');
 
 noteRouter.get('/', notesController.getALLNote);
+noteRouter.get('/archive', notesController.getAllArchiveNotes);
 noteRouter.get('/:id', notesController.getNotebyId);
 noteRouter.post('/', notesController.addNote);
 noteRouter.put('/:id', notesController.editnotebyID);
 noteRouter.delete('/:id', notesController.deletenote);
 noteRouter.post('/search', notesController.searchNote);
 noteRouter.post('/template',notesController.addWithTemplate);
+noteRouter.put('/archive/:id', notesController.archiveNote);
+noteRouter.get('/export/:id', notesController.exportNote);
+
 
 module.exports = {
     noteRouter
 };
+
