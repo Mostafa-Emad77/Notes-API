6f4f29e187cc9624e41a5a664a7a864ae6a208ee
youssefh00 <93389696+youssefh00@users.noreply.github.com>
2023-04-20 02:41:58 +0200

    adding import

--- routes/note_router.js
+++ routes/note_router.js
@@ -13,7 +13,7 @@ noteRouter.post('/search', notesController.searchNote);
 noteRouter.post('/template',notesController.addWithTemplate);
 noteRouter.put('/archive/:id', notesController.archiveNote);
 noteRouter.get('/export/:id', notesController.exportNote);
-
+noteRouter.post('/import', notesController.importNote);
 
 module.exports = {
     noteRouter
