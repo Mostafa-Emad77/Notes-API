da3a1aa062afec376da1d73c8046c75038ab5604
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-04-20 12:02:13 +0200

    edit

--- features/Search.js
+++ features/Search.js
@@ -1,16 +1,16 @@
 const { Note } = require('../models/note');
 const searchNote = async (req, res) => {
-    const searchTerm = req.body.term;
-    const searchProperty = req.body.prop || 'name';
+    const searchTerm = req.body.term;//the term to be search
+    const searchProperty = req.body.prop || 'name';//after or they are the default values
     const sortProperty = req.body.sort || 'id';
     const sortOrder = req.body.order || 'asc';
     const pinned = req.body.pinned;
-    const query = {};
+    const query = {};//the query is where the results will be 
   query[searchProperty] = { $regex: searchTerm, $options: 'i' };
-    if (pinned !== undefined) {
+    if (pinned !== undefined) {// see if the pinned is defined or not
         query['pinned'] = pinned;
       }     
-      const notes = await Note.find(query).sort({ [sortProperty]: sortOrder });
+      const notes = await Note.find(query).sort({ [sortProperty]: sortOrder });//notes take the results from the query 
       res.send(notes);
 };
 module.exports={ searchNote }
