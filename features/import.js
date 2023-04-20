2cb9b678a959a5c394916d2a2d97a9a6c6b244f5
Ibrahim Al-Gazzar <94261285+IbrahimGazzar@users.noreply.github.com>
2023-04-20 05:10:42 +0200

    Update import.js

--- features/import.js
+++ features/import.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 const importNote = async (req, res) => {
     try {
-      const filename = req.body.file; 
-      const filepath = path.join(__dirname, '..', filename); 
-      const data = fs.readFileSync(filepath, 'utf8');
-      const lines = data.trim().split('\n');
+      const filename = req.body.file;       //Input: Text file name (ex: note.txt)
+      const filepath = path.join(__dirname, '..', filename);    //Makes path of the text file
+      const data = fs.readFileSync(filepath, 'utf8');       //Reads the text inside the text file
+      const lines = data.trim().split('\n');        //Splits the lines and each of them as an element in the array
   
     const name = lines[0].substring(lines[0].indexOf(':') + 1).trim();
-    const description = lines[1].substring(lines[1].indexOf(':') + 1).trim();
-    const dueDate = lines[2].substring(lines[2].indexOf(':') + 1).trim();
+    const description = lines[1].substring(lines[1].indexOf(':') + 1).trim();       //Takes the needed data from each line and maps it to its own property 
+    const dueDate = lines[2].substring(lines[2].indexOf(':') + 1).trim();           //(name in line 0, and so on)
   
       const note = new Note({
         name,
@@ -20,7 +20,7 @@ const importNote = async (req, res) => {
         pinned: false, // default value
       });
       
-      await note.save();
+      await note.save(); //adds the created note to the database
   
       console.log(`Imported 1 note from ${path.basename(filepath)}.`);
       res.status(200).send(`Imported 1 note from ${path.basename(filepath)}.`);
